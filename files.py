f2027243a2ef00dfeecd9f1bddbd72b90a9a02d8
lshapz <laurashapiro01@gmail.com>
2017-03-28 16:23:12 -0400

    array cardio 2

--- files.py
+++ files.py
@@ -1,11 +1,11 @@
 import os
 import string
 
-# for root, dirs, files in os.walk('.'):
-#     for fname in files:
-#         if fname.endswith('ED.html'):
-#             os.remove(os.path.join(root, fname))
-# # removes index-FINISHED.html files -aka the answers
+for root, dirs, files in os.walk('.'):
+    for fname in files:
+        if fname.endswith('ED.html') or fname.endswith('ED.js'):
+            os.remove(os.path.join(root, fname))
+# removes FINISHED files -aka the answers
 
 # for fileName in os.listdir("."):
 #   if fileName[0].isdigit():
