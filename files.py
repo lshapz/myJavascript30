55f6d63536a663bffee323ea36b5632b785a57c5
lshapz <laurashapiro01@gmail.com>
2017-03-28 16:18:48 -0400

    python phile phun

---
+++ files.py
@@ -0,0 +1,13 @@
+import os
+import string
+
+# for root, dirs, files in os.walk('.'):
+#     for fname in files:
+#         if fname.endswith('ED.html'):
+#             os.remove(os.path.join(root, fname))
+# # removes index-FINISHED.html files -aka the answers
+
+# for fileName in os.listdir("."):
+#   if fileName[0].isdigit():
+#     os.rename(fileName, fileName.replace(" ", ""))
+# # removes whitespace from folders 
